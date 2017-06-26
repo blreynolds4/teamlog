a3a653b174d990f5d8dcd943853d96740a3ba0fd
Brian Reynolds <breynolds@renesys.com>
2017-06-26 17:07:26 -0600

    Modal dialogs and restoring data for submitted data.

--- home/views.py
+++ home/views.py
@@ -3,6 +3,9 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from tagging.utils import edit_string_for_tags
 from posts.models import TeamPost
+from datetime import date
+
+DATE_FORMAT = "%m-%d-%Y"
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
@@ -10,7 +13,7 @@ class HomeView(LoginRequiredMixin, TemplateView):
 
     def get(self, request):
         tags = edit_string_for_tags(request.user.userprofile.tags)
-        print("User", request.user.username, "User tags", tags)
         posts = TeamPost.tagged.with_any(tags)
 
-        return render(request, self.template_name, dict(posts=posts))
+        today = date.today().strftime(DATE_FORMAT)
+        return render(request, self.template_name, dict(posts=posts, today=today))
--- posts/views.py
+++ posts/views.py
@@ -3,33 +3,67 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from tagging.models import Tag
 from tagging.utils import edit_string_for_tags
+from datetime import datetime, date
 
 from .parsing import parse_duration
 from .models import TeamPost, RunPost
+
+DATE_FORMAT = "%m-%d-%Y"
+
+
+def parse_date(date_str):
+    dt = datetime.strptime(date_str, DATE_FORMAT)
+    return dt.date()
+
+
+def save_submitted_run_data(values):
+    result = dict()
+    result['today'] = values.get('run_date', '')
+    result['distance'] = values.get('run_distance', '')
+    result['duration'] = values.get('run_duration', '')
+    result['route'] = values.get('run_route', '')
+    result['description'] = values.get('run_description', '')
+
+    return result
+
+
+def save_submitted_msg_data(values):
+    result = dict()
+    result['message'] = values.get('message', '')
+
+    return result
 
 
 class RunView(LoginRequiredMixin, TemplateView):
     template_name = "posts/edit_run.html"
 
     def get(self, request):
-        return render(request, 'posts/edit_run.html')
+        return render(request, 'posts/edit_run.html', dict(today=date.today()))
 
     def post(self, request):
-        if request.POST['run_distance'] and request.POST['run_time']:
-            run = RunPost(author=request.user,
-                          duration=parse_duration(request.POST['run_time']),
-                          distance=float(request.POST['run_distance']),
-                          message=request.POST['run_description'],
-                          post_date=request.POST['run_date'])
-            run.save()
-            print("User", request.user.username, "Tags", edit_string_for_tags(request.user.userprofile.tags))
-            Tag.objects.update_tags(run.teampost_ptr, edit_string_for_tags(request.user.userprofile.tags))
+        run = save_submitted_run_data(request.POST)
+        try:
+            post_date = parse_date(request.POST['run_date'])
 
-            return redirect("home")
-        else:
+            if request.POST['run_distance'] and request.POST['run_time']:
+                run = RunPost(author=request.user,
+                              duration=parse_duration(request.POST['run_time']),
+                              distance=float(request.POST['run_distance']),
+                              message=request.POST['run_description'],
+                              post_date=post_date)
+                run.save()
+                print("User", request.user.username, "Tags", edit_string_for_tags(request.user.userprofile.tags))
+                Tag.objects.update_tags(run.teampost_ptr, edit_string_for_tags(request.user.userprofile.tags))
+
+                return redirect("home")
+            else:
+                return render(request,
+                              self.template_name,
+                              dict(error="Run requires at least time and distance.", run=run))
+        except ValueError:
             return render(request,
                           self.template_name,
-                          dict(error="ERROR:  Run requires at least time and distance."))
+                          dict(error="Date format must be 01-01-2017 (leading zeroes required)", run=run))
 
 
 class MessageView(LoginRequiredMixin, TemplateView):
@@ -39,16 +73,24 @@ class MessageView(LoginRequiredMixin, TemplateView):
         return render(request, 'posts/edit_msg.html')
 
     def post(self, request):
-        if request.POST['message']:
-            msg = TeamPost(author=request.user,
-                           message=request.POST['message'],
-                           post_date=request.POST['msg_date'])
-            msg.save()
-            print("User", request.user.username, "Tags", edit_string_for_tags(request.user.userprofile.tags))
-            Tag.objects.update_tags(msg, edit_string_for_tags(request.user.userprofile.tags))
+        msg = save_submitted_msg_data(request.POST)
+        try:
+            post_date = parse_date(request.POST['msg_date'])
+            if request.POST['message']:
+                msg = TeamPost(author=request.user,
+                               message=request.POST['message'],
+                               post_date=post_date)
+                msg.save()
+                print("User", request.user.username, "Tags", edit_string_for_tags(request.user.userprofile.tags))
+                Tag.objects.update_tags(msg, edit_string_for_tags(request.user.userprofile.tags))
 
-            return redirect("home")
-        else:
+                return redirect("home")
+            else:
+                return render(request,
+                              self.template_name,
+                              dict(error="Message is required.", msg=msg))
+        except ValueError:
             return render(request,
                           self.template_name,
-                          dict(error="ERROR:  Message is required."))
+                          dict(error="Date format must be 01-01-2017 (leading zeroes required)",
+                               msg=msg))
