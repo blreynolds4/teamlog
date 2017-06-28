0b64ad5cd46b494923b6b3a661d41709dcc81138
Brian Reynolds <breynolds@renesys.com>
2017-06-28 19:16:44 -0400

    bump version to 0.0.4

--- runchat/settings.py
+++ runchat/settings.py
@@ -19,7 +19,7 @@ print("Exec Env is", ExecutionEnvironment)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-APP_VERSION = "0.0.3"
+APP_VERSION = "0.0.4"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
